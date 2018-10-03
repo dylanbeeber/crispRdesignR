21be58d3e72269c33addbf4bee7dbd8037b464e5
dylanbeeber <38253997+dylanbeeber@users.noreply.github.com>
2018-10-03 17:11:56 -0400

    Removed TTTT homopolymer calling

--- R/getsgRNAdata.R
+++ R/getsgRNAdata.R
@@ -1,5 +1,5 @@
 getsgRNAdata <- function(alldata){
-  sgRNA_frame <- data.frame(alldata[1:15])
+  sgRNA_frame <- data.frame(alldata[1:14])
   colnames(sgRNA_frame) <- c("sgRNA sequence", "PAM sequence", "Direction", "Start", "End", "GC content",
                                       "TTTT Homopolymer", "Homopolymer", "Self Complementary", "Doench Score", "MM0", "MM1", "MM2", "MM3", "MM4")
   sgRNA_frame
