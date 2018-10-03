7b1108cb2d9f22e9bb75f93d3e69f45b05174919
dylanbeeber <38253997+dylanbeeber@users.noreply.github.com>
2018-10-03 17:11:32 -0400

    Removed TTTT homopolymer calling

--- R/getofftargetdata.R
+++ R/getofftargetdata.R
@@ -1,5 +1,5 @@
 getofftargetdata <- function(alldata){
-  off_targ_frame <- data.frame(alldata[16:27])
+  off_targ_frame <- data.frame(alldata[15:26])
   colnames(off_targ_frame) <- c("sgRNA sequence", "Chromosome", "Start", "End", "Mismatches", "Direction", "CFD Scores",
                                 "Off-target sequence", "Gene ID", "Gene Name", "Sequence Type", "Exon Number")
   off_targ_frame
