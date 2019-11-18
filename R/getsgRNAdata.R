179aed6c4334f1e19b680321d8d911fc2b64f3ac
dylanbeeber <38253997+dylanbeeber@users.noreply.github.com>
2019-11-17 22:00:59 -0500

    Removed TTTT homopolymer calling
    Removed a leftover reference to TTTT homopolymer calling

--- R/getsgRNAdata.R
+++ R/getsgRNAdata.R
@@ -1,6 +1,6 @@
 getsgRNAdata <- function(alldata){
   sgRNA_frame <- data.frame(alldata[1:14])
   colnames(sgRNA_frame) <- c("sgRNA sequence", "PAM sequence", "Direction", "Start", "End", "GC content",
-                                      "TTTT Homopolymer", "Homopolymer", "Self Complementary", "Doench Score", "MM0", "MM1", "MM2", "MM3", "MM4")
+                                      "Homopolymer", "Self Complementary", "Doench Score", "MM0", "MM1", "MM2", "MM3", "MM4")
   sgRNA_frame
 }
