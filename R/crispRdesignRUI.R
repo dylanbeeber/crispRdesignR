84e29849b8318851d449f328fbcaec1c87dcc4b1
dylanbeeber <38253997+dylanbeeber@users.noreply.github.com>
2020-05-10 16:15:17 -0400

    Add files via upload

--- R/crispRdesignRUI.R
+++ R/crispRdesignRUI.R
@@ -2,6 +2,7 @@
 
 ##crispRdesignRUI
 crispRdesignRUI <- function() {
+  requireNamespace("gbm", quietly = TRUE)
   app_path <- system.file("apps", "RunShiny.R", package = "crispRdesignR")
   shiny::runApp(app_path)
 }
